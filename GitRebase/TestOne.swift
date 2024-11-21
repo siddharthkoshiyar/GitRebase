6c313749552e2e6d0e5463335c531c293af6217a
siddharthkoshiyar <siddharthkoshiyar94@gmail.com>
2024-11-21 21:22:30 +0530

    - Added Test One Files

---
+++ GitRebase/TestOne.swift
@@ -0,0 +1,15 @@
+//
+//  TestOne.swift
+//  GitRebase
+//
+//  Created by SIDDHARTH KOSHIYAR on 21/11/24.
+//
+
+import Foundation
+
+
+struct TestOne {
+    let name: String
+    let address: String
+    let rollNumber: String
+}
