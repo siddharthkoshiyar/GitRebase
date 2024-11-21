bf459ed9c0d8af4dce3f2801b245d4a8f4a0b2b2
siddharthkoshiyar <siddharthkoshiyar94@gmail.com>
2024-11-21 21:15:09 +0530

    - Added Project In Github

---
+++ GitRebase/GitRebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitRebaseApp.swift
+//  GitRebase
+//
+//  Created by SIDDHARTH KOSHIYAR on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GitRebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
